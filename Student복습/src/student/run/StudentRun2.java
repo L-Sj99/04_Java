05243c258aae1dd5b26da287fe324f2897f6ac03
L-sj99 <yms6897@naver.com>
2024-07-22 17:40:06 +0900

    20240722_매소드, 객체배열 및 연습문제.

---
+++ Student복습/src/student/run/StudentRun2.java
@@ -0,0 +1,10 @@
+package student.run;
+
+import student.view.StudentView2;
+
+public class StudentRun2 {
+	public static void main(String[] args) {
+	StudentView2 view = new StudentView2();
+		view.mainMenu();
+	}
+}
---
+++ Student복습/src/student/view/StudentView2.java
@@ -0,0 +1,8 @@
+package student.view;
+
+import java.util.Scanner;
+
+public class StudentView2 {
+	private Scanner sc = new Scanner(System.in);
+	
+}
